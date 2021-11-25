4a1d85dffeecc29b79534dab331020bc6b7a7622
Antonio Ochoa <antonhub00@gmail.com>
2021-11-24 20:31:25 -0600

    feat: documentation use cases added

---
+++ src/Documentation/Domain/UseCases/DocumentationUseCasesHandler.ts
@@ -0,0 +1,30 @@
+import IDocumentationDTO from "./Contracts/IDocumentationDTO";
+import IDocumentationRepository from "./Contracts/IDocumentationRespository";
+
+export default class DocumentationUseCasesHandler {
+  private repository: IDocumentationRepository;
+
+  constructor(respository: IDocumentationRepository) {
+    this.repository = respository;
+  }
+
+  public async create(documentation: IDocumentationDTO): Promise<void> {
+    await this.repository.create(documentation);
+  }
+
+  public async findById(id: string): Promise<IDocumentationDTO | undefined> {
+    return await this.repository.findById(id);
+  }
+
+  public async findAll(): Promise<IDocumentationDTO[] | undefined> {
+    return await this.repository.findAll();
+  }
+
+  public async update(documentation: IDocumentationDTO): Promise<void> {
+    await this.repository.update(documentation);
+  }
+
+  public async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}

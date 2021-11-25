dc2e46618ae8a427d8a44178c7cff1bf6fe495e7
Antonio Ochoa <antonhub00@gmail.com>
2021-11-24 20:22:58 -0600

    feat!: documentation db entity id type changed from string to number

--- src/Documentation/Infrastructure/Database/DBEntities/DocumentationDBEntity.ts
+++ src/Documentation/Infrastructure/Database/DBEntities/DocumentationDBEntity.ts
@@ -1,9 +1,9 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("Documentation")
 export default class DocumentationDBEntity {
-  @PrimaryColumn()
-  id!: string;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
   @Column()
   name!: string;

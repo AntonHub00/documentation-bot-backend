4dae2f8d62257101f7ecb5e07c19cd3baeac904c
Antonio Ochoa <antonhub00@gmail.com>
2021-11-24 21:26:08 -0600

    feat: index.ts udpated to configure and start the Express JS server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 import "reflect-metadata";
+import express, { Express } from "express";
+
+import dbConnection from "./Documentation/Infrastructure/Database/DBConnection";
+
+const startServer = async () => {
+  await dbConnection;
+
+  // Need to wait for connection to be ready because controllers imports the
+  // dependency injection module which get repository specific implementations.
+  // If the connection is not ready, trying to get the repositories will fail.
+  const { default: DocumentationController } = await import(
+    "./Documentation/Infrastructure/Controllers/DocumentationController"
+  );
+
+  const port = 5000;
+
+  const app: Express = express();
+
+  app.use(express.json());
+  app.use("/api/documentation", new DocumentationController().router);
+
+  app.listen(port, () =>
+    console.log(`Server listening on http://localhost:${port}`)
+  );
+};
+
+startServer();
